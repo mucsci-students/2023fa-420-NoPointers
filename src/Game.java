f0137037af9842bc29e9a342c05d02363e33e926
MarauderNick <143138555+MarauderNick@users.noreply.github.com>
2023-09-07 14:59:16 -0400

    Setup Initial Eclipse Project

---
+++ src/Game.java
@@ -0,0 +1,8 @@
+
+// Command Lines to call on classes. Puzzle class, GameState class, Dictionary class.
+public class Game {
+	public static void main(String[] args) {
+		System.out.println("Hello, World!"); 
+	}
+	
+} 

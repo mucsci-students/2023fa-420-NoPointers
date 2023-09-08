ee6180374a737e1d24bae91a678647e9960fa458
MarauderNick <143138555+MarauderNick@users.noreply.github.com>
2023-09-08 17:23:35 -0400

    Update Puzzle.java

--- src/Puzzle.java
+++ src/Puzzle.java
@@ -5,22 +5,23 @@ public class Puzzle {
 	private String[] letters;
 	// The required letter
 	private String requiredLetter;
-	
+		
 	// Builder (New puzzle)
 	public Puzzle() {
 		// Add 6 random letters to letters array
-		
+			
 		// Insert a random vowel for required letter
-		
+			
 	}
-	
+		
 	// Builder using input from user (New puzzle from base)
 	public Puzzle(String input) {
 		// Take 6 non-requited letters from input
-		
+			
 		// Take requiredLetter from input (Have user specify what is required letter?
 		// Or just make a system like having the last letter be the requited letter?
-		
+			
 	}
 	
 }
+

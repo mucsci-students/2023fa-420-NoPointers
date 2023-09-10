d62efd489699b7e00439ac53c469f7ff8bc74b58
MarauderNick <143138555+MarauderNick@users.noreply.github.com>
2023-09-10 13:07:41 -0400

    Updates for Puzzle Class
    Completed the constructor of a new puzzle from scratch. Created helper methods to get 6 random letters and a random vowel. Completed showPuzzle method to printout the 7 letters to user. Added template for guessWord method and shuffleLetters method to be filled in later. Still need to finish the constrcutor that uses input from the user & need to find a way to make sure that the required vowel isn't already part of the 6 previous letters.

--- src/Game.java
+++ src/Game.java
@@ -3,6 +3,9 @@
 public class Game {
 	public static void main(String[] args) {
 		System.out.println("Hello, World!"); 
+		// Testing puzzle used to verify methods work
+		Puzzle p = new Puzzle();
+		
 	}
 	
 } 

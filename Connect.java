f1bca4ada41f5fa203a582093b85bdb40510e996
yohannesgeleta <143038217+yohannesgeleta@users.noreply.github.com>
2023-09-12 14:53:50 -0400

    Update Connect.java

--- Connect.java
+++ Connect.java
@@ -5,41 +5,37 @@ import java.sql.Statement;
 import java.util.Scanner;
 
 public class Connect {
-	public static void main(String[] args) {
+	public boolean acsess(String word) {
         // Database URL (You can specify the path where you want to create the database)
         String url = "jdbc:sqlite:/path/to/your/database.db";
-        Scanner console = new Scanner(System.in);
-        String wordString = console.next();
-        int size = wordString.length();
-        
+        int size = word.length();
         try {
             // Create a connection to the database
             Connection conn = DriverManager.getConnection(url);
-
             if (conn != null) {
                 System.out.println("Connected to the database");
-                findWord(size, wordString, conn);
+                boolean res = findWord(size, word, conn);
+                
                 conn.close();
-                
-                
+                System.out.println("Database and table created successfully");
             }
         } catch (SQLException e) {
-		System.out.println("Acsess not granted. Didn't work");
+            System.err.println("Database creation error: " + e.getMessage());
         }
-        
+        return res;
     }
 	
 	public static boolean findWord(int size, String wordString,Connection conn) {
 		if(size < 4) {
 			return false;
 		}
-		String num = Integer.toString(size);
+		String num = String.valueOf(size);
 		Statement stmt = conn.createStatement();
 
-        // SQL Query to find word in table of specific word size
-        String SQLQuery = "SELECT * FROM " + num + "words WHERE word = \"" 
-        		+ wordString +"\";";
-        stmt.execute(SQLQuery);
+        // Create a table (You can define your own schema)
+        String SQLQuery = "SELECT * FROM " + num + "words WHERE word = '" 
+        		+ wordString +"';";
+        stmt.executequery(SQLQuery);
         stmt.close();
 		return true;
 	}

78edc3a55c8bf271af62c9398678d647a8d0bce8
Kyler Stigelman <45629870+kstigelman@users.noreply.github.com>
2023-09-12 15:00:10 -0400

    Correctly connects to sqlite and queries for access

--- Connect.java
+++ Connect.java
@@ -5,38 +5,66 @@ import java.sql.Statement;
 import java.util.Scanner;
 
 public class Connect {
-	public boolean acsess(String word) {
-        // Database URL
-        String url = "jdbc:sqlite:/path/to/your/database.db";
-        int size = word.length();
-        try {
-            // Create a connection to the database
-            Connection conn = DriverManager.getConnection(url);
-            if (conn != null) {
-                System.out.println("Connected to the database");
-                boolean res = findWord(size, word, conn);
-                
-                conn.close();
-                System.out.println("Database and table created successfully");
-            }
-        } catch (SQLException e) {
-            System.err.println("Database creation error: " + e.getMessage());
-        }
-        return res;
-    }
+	public static String[] numToWords = {
+			"zero",
+			"one",
+			"two",
+			"three",
+			"four",
+			"five",
+			"six",
+			"seven",
+			"eight",
+			"nine",
+			"ten",
+			"eleven",
+			"twelve",
+			"thirteen",
+			"fourteen",
+			"fifteen"
+	};
+	
+	public boolean access(String word) {
+		// Database URL
+		String url = "jdbc:sqlite:src/words";
+		int size = word.length();
+
+		boolean res = false;
+		try {
+		    // Create a connection to the database
+		    Connection conn = DriverManager.getConnection(url);
+		    if (conn != null) {
+			System.out.println("Connected to the database");
+			res = findWord(size, word, conn);
+			
+			conn.close();
+			System.out.println("Database and table created successfully");
+		    }
+		} catch (SQLException e) {
+		    System.err.println("Database creation error: " + e.getMessage());
+		}
+		return res;
+	}
 	
 	public static boolean findWord(int size, String wordString,Connection conn) {
 		if(size < 4) {
 			return false;
 		}
-		String num = String.valueOf(size);
-		Statement stmt = conn.createStatement();
+		
+		Statement stmt;
+		try {
+			stmt = conn.createStatement();
+			 String SQLQuery = "SELECT * FROM " + numToWords[size] + "words WHERE word = \"" 
+		        		+ wordString +"\";";
+		        ResultSet rs = stmt.executeQuery (SQLQuery);
+		        boolean found = rs.next ();
+		        stmt.close();
+		        return found;
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
-        // SQL Query to find word in table
-        String SQLQuery = "SELECT * FROM " + num + "words WHERE word = '" 
-        		+ wordString +"';";
-        stmt.executequery(SQLQuery);
-        stmt.close();
-	return true;
+		return false;
 	}
 }

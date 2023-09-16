a8da841a27d67f894b7a1ef0bd2ee74bf66e7450
csmichel <69485224+hihi4605@users.noreply.github.com>
2023-09-16 12:43:25 -0400

    Add files via upload
    The most recent version of the CLI including the version of the program that we showed off

---
+++ src/Connect.java
@@ -0,0 +1,208 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
+import org.sqlite.Function;
+
+public class Connect {
+	public static String[] numToWords = {
+			"zero",
+			"one",
+			"two",
+			"three",
+			"four",
+			"five",
+			"six",
+			"seven",
+			"eight",
+			"nine",
+			"ten",
+			"eleven",
+			"twelve",
+			"thirteen",
+			"fourteen",
+			"fifteen"
+	};
+	static char[] alphabet = {
+			'a',
+			'b',
+			'c',
+			'd',
+			'e',
+			'f',
+			'g',
+			'h',
+			'i',
+			'j',
+			'k',
+			'l',
+			'm',
+			'n',
+			'o',
+			'p',
+			'q',
+			'r',
+			's',
+			't',
+			'u',
+			'v',
+			'w',
+			'x',
+			'y',
+			'z'
+	};
+	
+	public static String getSqlString (char[] letters) {
+		ArrayList<Character> nots = new ArrayList<>();
+		ArrayList<Character> characters = new ArrayList<>();
+		
+		for (int i = 0; i < letters.length; ++i) {
+			nots.add (letters[i]);
+		}
+		for (int i = 0; i < 26; ++i) {
+			if (!nots.contains(alphabet[i]))
+				characters.add(alphabet[i]);
+		}
+		
+		String sql = "SELECT * FROM words WHERE word LIKE '%" + letters[6] + "%'";
+		for (int i = 0; i < characters.size (); ++i) {
+			sql += " AND word NOT LIKE '%" + characters.get(i) + "%'";
+		}
+		sql += ";";
+		return sql;
+	}
+	// Get words should be used to 
+	public static ArrayList<String> getWords (char[] letters) {
+		String url = "jdbc:sqlite:src/words";
+		ArrayList<String> wordList = new ArrayList<>();
+		try {
+		    // Create a connection to the database
+		    Connection conn = DriverManager.getConnection(url);
+		   
+		    Function.create(conn, "REGEXP", new Function() {
+                @Override
+                protected void xFunc() throws SQLException {
+                    String expression = value_text(0);
+                    String value = value_text(1);
+                    
+                    if (value == null)
+                        value = "";
+                   
+                    Pattern pattern=Pattern.compile(expression);
+                    result(pattern.matcher(value).find() ? 1 : 0);
+                }
+            });
+		    
+		    if (conn != null) {
+		    	Statement stmt;
+			
+				stmt = conn.createStatement();
+			
+		        ResultSet rs = stmt.executeQuery (getSqlString (letters));
+		        
+		        while (rs.next ()) {
+		        	wordList.add (rs.getString (1));
+		        }
+		        
+		        stmt.close();
+				
+				conn.close ();
+		    }
+		} catch (SQLException e) {
+		    System.err.println("Database creation error: " + e.getMessage());
+		}
+		return wordList;
+	}
+	public static void search (int size, String letters) {
+		String url = "jdbc:sqlite:src/words";
+		
+		try {
+		    // Create a connection to the database
+		    Connection conn = DriverManager.getConnection(url);
+		   
+		    Function.create(conn, "REGEXP", new Function() {
+                @Override
+                protected void xFunc() throws SQLException {
+                    String expression = value_text(0);
+                    String value = value_text(1);
+                    
+                    if (value == null)
+                        value = "";
+                   
+
+                    Pattern pattern=Pattern.compile(expression);
+                    boolean b = pattern.matcher(value).find();
+                    
+                    result(b ? 1 : 0);
+                }
+            });
+		    
+		    if (conn != null) {
+		    	Statement stmt;
+			
+				stmt = conn.createStatement();
+				String SQLQuery = letters;
+				
+		        ResultSet rs = stmt.executeQuery (letters);
+		        int count = 0;
+		        while (rs.next ()) {
+		        	System.out.print (rs.getString (1) + ", ");
+		        	++count;
+		        }
+		        System.out.print(count);
+		        stmt.close();
+				
+				conn.close ();
+		    }
+		} catch (SQLException e) {
+		    System.err.println("Database creation error: " + e.getMessage());
+		}
+		
+	}
+	public static boolean access(String word) {
+		// Database URL
+		String url = "jdbc:sqlite:src/words";
+		int size = word.length();
+
+		boolean res = false;
+		try {
+		    // Create a connection to the database
+		    Connection conn = DriverManager.getConnection(url);
+		    if (conn != null) {
+			res = findWord(size, word, conn);
+			
+			conn.close();
+		    }
+		} catch (SQLException e) {
+		    System.err.println("Database creation error: " + e.getMessage());
+		    return false;
+		}
+		return res;
+	}
+	
+	public static boolean findWord(int size, String wordString,Connection conn) {
+		if(size < 4) {
+			return false;
+		}
+		
+		Statement stmt;
+		try {
+			stmt = conn.createStatement();
+			 String SQLQuery = "SELECT * FROM " + numToWords[size] + "words WHERE word = \"" 
+		        		+ wordString +"\";";
+		        ResultSet rs = stmt.executeQuery (SQLQuery);
+		        boolean found = rs.next ();
+		        stmt.close();
+		        return found;
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			//e.printStackTrace();
+			return false;
+		}
+	}
+}

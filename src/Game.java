ef59837ba1a7dc3b529d2b0fd2c868cd1e0b6772
csmichel <69485224+hihi4605@users.noreply.github.com>
2023-09-17 18:19:15 -0400

    Update Game.java Refactoring

--- src/Game.java
+++ src/Game.java
@@ -1,11 +1,11 @@
-
 // Command Lines to call on classes. Puzzle class, GameState class, Dictionary class.
 public class Game {
 	public static void main(String[] args) {
-		System.out.println("Hello, World!"); 
-		// Testing puzzle used to verify methods work
-		Puzzle p = new Puzzle();
-		System.out.println (Connect.access("molly"));
+		Connect.access("molly");
+		CLI c = new CLI();
+		System.out.println("\033[49m");
+ 
+
 	}
-	
-} 
+
+}

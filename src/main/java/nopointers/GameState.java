cd6b01fd5586342ce0bf0ffd7ee4d87c3dcdb42e
Kyler P Stigelman <kpstigel@simon.muad.local>
2023-11-06 17:30:36 -0500

    Lowered amount of testing

--- src/main/java/nopointers/GameState.java
+++ src/main/java/nopointers/GameState.java
@@ -131,7 +131,7 @@ public class GameState {
     public boolean time(boolean fail) {
         try {
             if (fail)
-                TTimeUnit.MILLISECONDS.sleep(2000);
+                TimeUnit.MILLISECONDS.sleep(2000);
             for (int i = 0; i < 100; ++i) {
                 TimeUnit.MILLISECONDS.sleep(1);
                 System.out.print("\u001b[1000D");
